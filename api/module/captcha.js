c994ddc31a3d044920298254283f895d79f5b44b
Leo <alin.run@foxmail.com>
2021-08-07 02:55:24 +0800

    feat: 添加后台登陆调用

---
+++ api/module/captcha.js
@@ -0,0 +1,18 @@
+import request from '../../util/api.js';
+
+// 接口
+const Api = {
+	create: '/api/captcha/create'
+}
+
+// 创建
+const create = () => {
+	// console.log(request);
+	return request.request({
+		url: Api.create
+	})
+}
+
+export default {
+	create
+}
